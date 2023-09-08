7cdff330d2a31ab830179bde7cd8e9fb058c4a5f
Shin Umeda <umeda.shin@gmail.com>
2023-09-07 21:13:35 -0700

    selected chip

--- pico.py
+++ pico.py
@@ -5,6 +5,7 @@ spi = SPI(sck=Pin(2), mosi=Pin(4), miso=Pin(5))
 cs = Pin(3, mode=Pin.OUT, value=1) 
 bme = BME680_I2C(spi)
 
+cs(0)
 while(True):
     with open("out.txt", "a") as f:
         print(f"{time.time()}, {bme.temperature}, {bme.humidity}, {bme.pressure}, {bme.gas};", file = f)
